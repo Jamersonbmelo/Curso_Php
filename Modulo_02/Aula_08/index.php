c42d76c21c38aa03243b0113d7b96e9bc9cec37d
jamersonbmelo <jamersonjosealvaro@gmail.com>
2023-10-24 11:10:09 -0300

    estudo24_10_23

---
+++ Modulo_02/Aula_08/index.php
@@ -0,0 +1,10 @@
+<?php
+$traco = '-';
+$numero = 0;
+
+while($numero < 10){
+    echo str_repeat($traco,10).'<br/>';
+    $numero++;
+}
+
+?>
---
+++ Modulo_02/Aula_09/index.php
@@ -0,0 +1,10 @@
+<?php
+$traco = "-";
+
+for ( $i = 0; $i != 20; $i++)
+{
+    echo $traco . "<br/>";
+    $traco .= "-";
+}
+
+?>
